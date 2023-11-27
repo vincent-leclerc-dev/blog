4de9c46b8f7d72a7b3ac2c83f16e08b727b34679
Vincent <vleclerc.dev@gmail.com>
2023-11-27 14:04:11 +0100

    chore(config): tools configuration

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+const Navbar = () => (
+  <nav>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/">Blog</a>
+      </li>
+      <li>
+        <a href="/about">Github</a>
+      </li>
+      <li>
+        <a href="/contact">Twitter</a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export { Navbar };
