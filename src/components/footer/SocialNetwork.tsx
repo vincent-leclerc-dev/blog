8a92a4c519ac106f8a9ce49ff6181bf67f198ba2
Vincent <vleclerc.dev@gmail.com>
2023-12-07 17:52:32 +0100

    style(footer): add tooltip on social networks

--- src/components/footer/SocialNetwork.tsx
+++ src/components/footer/SocialNetwork.tsx
@@ -12,17 +12,25 @@ interface Props {
 
 export const SocialNetwork = ({ config }: Props): JSX.Element => {
   return (
-    <a
-      className="relative h-[30px] w-[30px]"
-      href={`https://www.${config.platform}.com/${config.username}`}
-      target="_blank"
-    >
-      <img
+    <div className="group relative flex">
+      <a
         className="relative h-[30px] w-[30px]"
-        alt={config.platform}
-        src={`/${config.platform}.svg`}
-      />
-    </a>
+        href={`https://www.${config.platform}.com/${config.username}`}
+        target="_blank"
+      >
+        <img
+          className="relative h-[30px] w-[30px]"
+          alt={config.platform}
+          src={`/${config.platform}.svg`}
+        />
+      </a>
+      <span
+        className="absolute left-1/2 m-2 mx-auto -translate-x-1/2 translate-y-full rounded-xl bg-blue p-2 text-sm 
+      font-semibold capitalize text-white opacity-0 transition-opacity group-hover:opacity-100"
+      >
+        {config.platform}
+      </span>
+    </div>
   );
 };
 
