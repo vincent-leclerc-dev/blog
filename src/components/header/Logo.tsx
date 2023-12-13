c6e5b791ebcf41bf2ffa6a3f703efad608cf13d3
Vincent <vleclerc.dev@gmail.com>
2023-12-13 01:50:13 +0100

    style(header): accessibility

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,9 +1,13 @@
 export const Logo = (): JSX.Element => (
   <a
+    aria-label="Logo"
     href="/"
     className="absolute left-0 top-0 z-30 m-[18px] flex h-[48px] w-[48px] flex-col"
   >
-    <div className="group absolute left-0 top-0 z-40 h-[48px] w-[48px] bg-[url('/logo-48-48-holo-black.svg')]">
+    <div
+      aria-hidden="true"
+      className="group absolute left-0 top-0 z-40 h-[48px] w-[48px] bg-[url('/logo-48-48-holo-black.svg')]"
+    >
       &nbsp;
     </div>
   </a>
